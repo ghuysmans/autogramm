0adb0744f3440e34d13fbd4196f7817b2c2e42b6
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-05-16 22:23:16 +0200

    Computed the axiom and a rule dictionary

--- parser.py
+++ parser.py
@@ -2,54 +2,84 @@ import sys
 from ply import yacc
 from lexer import lex, tokens
 
+rules = {}
+
 precedence = (
 	('left', 'NL'),
 	('left', 'OR'),
 )
 
-def p_program(p):
+def p_programP(p):
 	'''
-	program	: PYTHON program
-			| block program
-			|
+	program : PYTHON program
+	'''
+	p[0] = p[2]
+
+def p_programB(p):
+	'''
+	program : block program
+	'''
+	p[0] = p[1]
+
+def p_programE(p):
+	'''
+	program :
 	'''
 	pass
 
 def p_block(p):
 	'''
-	block	: BLOCK NL rules BLOCK
+	block : BLOCK NL rules BLOCK
 	'''
-	pass
+	p[0] = p[3]
 
 def p_rules(p):
 	'''
 	rules	: rule rules
 			| rule
 	'''
-	pass
+	p[0] = p[1]
 
 def p_rule(p):
 	'''
-	rule	: VARIABLE COLON disj
+	rule : VARIABLE COLON disj
 	'''
-	pass
+	if p[1] not in rules:
+		rules[p[1]] = []
+	rules[p[1]].extend(p[3])
+	p[0] = p[1]
 
-def p_disj(p):
+def p_disjE(p):
 	'''
-	disj	: terms NL OR disj
-			| terms NL
-			| NL
+	disj : NL
 	'''
-	pass
+	p[0] = [None]
 
-def p_terms(p):
+def p_disjB(p):
+	'''
+	disj : terms NL
+	'''
+	p[0] = [p[1]]
+
+def p_disjI(p):
+	'''
+	disj : terms NL OR disj
+	'''
+	p[0] = [p[1]] + p[4]
+
+def p_termsB(p):
+	'''
+	terms	: VARIABLE
+			| TOKEN
+	'''
+	p[0] = [p[1]]
+
+def p_termsI(p):
 	'''
 	terms	: VARIABLE terms
 			| TOKEN terms
-			| VARIABLE
-			| TOKEN
 	'''
-	pass
+	p[0] = [p[1]] + p[2]
 
 def p_error(p):
 	where = "near line "+str(p.lineno)
@@ -60,4 +90,6 @@ yacc = yacc.yacc()
 if __name__=="__main__":
 	import sys
 	inp = sys.stdin.read()
-	yacc.parse(inp, debug=("-d" in sys.argv))
+	print yacc.parse(inp, debug=("-d" in sys.argv))
+	for r in rules:
+		print r, rules[r]
