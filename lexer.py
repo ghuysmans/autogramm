3500eb48ec31807fa60d2c49b0b7784b56203e24
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-05-17 02:44:55 +0200

    Ignored %prec directives

--- lexer.py
+++ lexer.py
@@ -23,6 +23,9 @@ t_gramm_VARIABLE = '[a-z_][a-zA-Z0-9_]*'
 t_gramm_COLON = ':'
 t_gramm_TOKEN = '[A-Z_][A-Z0-9_]*'
 t_gramm_OR = r'\|'
+def t_gramm_PREC(t):
+	r'%prec\ [a-zA-Z0-9_]+' #spaces must be escaped because of re.VERBOSE
+	pass
 def t_gramm_NL(t):
 	r'\n+'
 	t.lexer.lineno += len(t.value)
